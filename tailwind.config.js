63f5947ad5014c390ff465fe39392a53b222ff65
unknown <deepimpact5@yahoo.com>
2021-06-18 23:52:03 -0400

    added bottom control panel which is almost complete

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: {
+        basic: '#ffffff'
+      },
+      black: {
+        basic: '#000000'
+      },
+      gray: {
+        home_charcoal1: '#161616',
+        home_charcoal2: '#0e0e0e',
+        see_more: '#9e9e9e',
+        home_tiles: '#333333',
+        artist: '#999999',
+        control_bg: '#353535',
+        controls: '#999999',
+        time_vol: '#323232',
+        elapsed: '#a9a9a9',
+
+      },
+      blue: {
+        new_blue: '#192055',
+        dark_teal: '#274767',
+        panel1: '#2b3946',
+        panel2: '#242a30',
+        mix_title: '#f9fbfc',  // or white?
+      },
+      red: {
+        top_upper: '#901b22',
+        top_lower: '#3f1113',
+        bottom_upper: '#340d0f',
+        almost_black: '#0e0e0e',
+      },
+      green: {
+        play: '#42ae43'
+      },
+
+    },
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

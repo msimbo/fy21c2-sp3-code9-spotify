05c6e6faec04ba19c99ef3c1c8f71a4860f7d1a2
unknown <deepimpact5@yahoo.com>
2021-06-29 17:33:57 -0400

    started working on login script

---
+++ src/login.js
@@ -0,0 +1,12 @@
+
+const loginBtn2 = document.getElementById('login2');
+
+loginBtn2.addEventListener('mouseup', () => {     // mouseup or "full click" ?
+    const username = document.getElementById('user');
+    const password = document.getElementById('pswd');
+
+    // @TODO check username and password
+
+    // @TODO if both ok then go to HOME screen (index.html)
+    // if not provide feedback
+});

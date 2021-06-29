a169b2facad3eb3005f6e900b326596e7e196c47
unknown <deepimpact5@yahoo.com>
2021-06-29 17:41:00 -0400

    wrote most of login script

--- src/login.js
+++ src/login.js
@@ -5,8 +5,29 @@ loginBtn2.addEventListener('mouseup', () => {     // mouseup or "full click" ?
     const username = document.getElementById('user');
     const password = document.getElementById('pswd');
 
-    // @TODO check username and password
-
-    // @TODO if both ok then go to HOME screen (index.html)
-    // if not provide feedback
+    if( validUN(username.value)  &&  validPW(password.value) ) {
+        console.log( "username & password accepted!" );
+        // @TODO take user to HOME screen (index.html)
+    }
+    else {
+        console.log( "username must be at least 7 characters; password must be at least 9 and contain a digit. please try again" );
+        // @TODO: provide visual feedback
+    }
 });
+
+function validUN(username) {
+    return (username.length > 6);
+}
+
+function validPW(password) {
+    return ( password.length > 8  &&  hasDigit(password) );
+}
+
+function hasDigit( myStr ) {
+    for( let i = myStr.length - 1; i > -1; i-- ) {
+        if(  Number(myStr.charAt(i)) >= 0  )
+            return true;
+    }
+    return false;
+}
+

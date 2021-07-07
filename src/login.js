e0f7c52d1598ea9f4c94e44a3ca934fff9a7f8d6
Casey Geist <deepimpact5@yahoo.com>
2021-07-07 16:32:08 -0400

    added a little color to login2

--- src/login.js
+++ src/login.js
@@ -1,25 +1,42 @@
-
+const loginForm = document.getElementById('login-form');
 const loginBtn2 = document.getElementById('login2');
+const yesBtn = document.getElementById('yes');
 
-loginBtn2.addEventListener('mouseup', () => {     // mouseup or "full click" ?
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();   // necessary to prevent submit button and form from jumping the gun
+
     const username = document.getElementById('user');
     const password = document.getElementById('pswd');
+    const criteriaMsg = document.getElementById('criteria');  // put here for simplicity instead of inside else
 
-    if( validUN(username.value)  &&  validPW(password.value) ) {
-        console.log( "username & password accepted!" );
-        // @TODO take user to HOME screen (index.html)
+    if( usernameValid(username.value)  &&  passwordValid(password.value) ) {
+        loginBtn2.innerText = "LOGGING IN...";
+        window.location.href = "index.html";
     }
     else {
-        console.log( "username must be at least 7 characters; password must be at least 9 and contain a digit. please try again" );
-        // @TODO: provide visual feedback
+            // show message about username/password requirements
+        criteriaMsg.classList.remove('delay-500','opacity-0');
+        criteriaMsg.classList.add('opacity-95');
+            // hide message upon click
+        criteriaMsg.addEventListener('click', () => {
+            criteriaMsg.classList.add('delay-500');
+            criteriaMsg.classList.remove('opacity-95');
+            criteriaMsg.classList.add('opacity-0');
+        });
     }
 });
 
-function validUN(username) {
+yesBtn.addEventListener('click', () => {
+    const signupFooter = document.getElementById('signup-login');
+    signupFooter.classList.add('hidden');
+})
+
+
+function usernameValid(username) {
     return (username.length > 6);
 }
 
-function validPW(password) {
+function passwordValid(password) {
     return ( password.length > 8  &&  hasDigit(password) );
 }
 

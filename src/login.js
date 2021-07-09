2d0f034d0bc5d7c9d473cff43fd50d7e4a1df4d8
Casey Geist <deepimpact5@yahoo.com>
2021-07-09 11:48:56 -0400

    finishing touches for SP4

--- src/login.js
+++ src/login.js
@@ -1,13 +1,16 @@
 const loginForm = document.getElementById('login-form');
 const loginBtn2 = document.getElementById('login2');
-const yesBtn = document.getElementById('yes');
+const username = document.getElementById('user');       // put here for simplicity instead of inside loginForm's event listener
+const password = document.getElementById('pswd');       // put here for simplicity instead of inside loginForm's event listener
+const criteriaMsg = document.getElementById('criteria');  // put here for simplicity instead of inside else, inside event listener
+
+const eyeDiv = document.getElementById('eye-div');      // extras
+const eye = document.getElementById('eye');             // extras
+let visible = false;                                             // extras
+const yesBtn = document.getElementById('yes');          // extras
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();   // necessary to prevent submit button and form from jumping the gun
-
-    const username = document.getElementById('user');
-    const password = document.getElementById('pswd');
-    const criteriaMsg = document.getElementById('criteria');  // put here for simplicity instead of inside else
 
     if( usernameValid(username.value)  &&  passwordValid(password.value) ) {
         loginBtn2.innerText = "LOGGING IN...";
@@ -26,20 +29,51 @@ loginForm.addEventListener('submit', (event) => {
     }
 });
 
+// EXTRA:  toggle password visibility
+eyeDiv.addEventListener('click', () => {
+    if(!visible) {
+        eye.innerText = "visibility";
+        visible = true;
+        password.setAttribute("type", "text");
+    }
+    else {
+        eye.innerText = "visibility_off";
+        visible = false;
+        password.setAttribute("type", "password");
+    }
+});
+
+// EXTRA:  When 'Yes' button is clicked, footer disappears
 yesBtn.addEventListener('click', () => {
     const signupFooter = document.getElementById('signup-login');
     signupFooter.classList.add('hidden');
-})
+});
 
 
+
+/**
+ *
+ * @param username
+ * @returns {boolean}
+ */
 function usernameValid(username) {
     return (username.length > 6);
 }
 
+/**
+ *
+ * @param password
+ * @returns {boolean}
+ */
 function passwordValid(password) {
     return ( password.length > 8  &&  hasDigit(password) );
 }
 
+/**
+ *
+ * @param myStr
+ * @returns {boolean}
+ */
 function hasDigit( myStr ) {
     for( let i = myStr.length - 1; i > -1; i-- ) {
         if(  Number(myStr.charAt(i)) >= 0  )
--- src/toggle.js
+++ src/toggle.js
@@ -1,18 +1,41 @@
-let playButton = document.getElementById('play-pause');
-let ppIcon = document.getElementById('pp-icon');
+const emilyButton = document.getElementById('emily');
+const dropDown = document.getElementById('dropdown');
+
+const heartDiv = document.getElementById('heart-box');
+const heart = document.getElementById('heart');
+let liked = false;
+
+const playButton = document.getElementById('play-pause');
+const ppIcon = document.getElementById('pp-icon');
 let playing = false;
 
+/* Show/Hide dropdown menu */
+emilyButton.addEventListener('click', () => {
+    dropDown.classList.toggle('hidden')
+});
+
+heartDiv.addEventListener( 'click', () => {
+    if(!liked) {
+        heart.innerText = "favorite";
+        liked = true;
+    }
+    else {
+        heart.innerText = "favorite_border";
+        liked = false;
+    }
+});
+
+/* Toggle PLAY/PAUSE button icon */
 playButton.addEventListener('click', () => {
     if(!playing) {
         // begin playing song
-        // start timer
-        // update elapsed time continuously
+        // start updating times and visual continuously
         ppIcon.innerText = "pause_circle_filled";
         playing = true;
     }
     else {
         // stop playing song
-        // stop timer
+        // stop updating time and the play progress bar
         ppIcon.innerText = "play_circle_filled";
         playing = false;
     }
